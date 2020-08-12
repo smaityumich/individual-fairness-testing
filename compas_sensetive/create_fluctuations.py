912fa4a58f4496c582845b79fc7eea0d1f7640d8
Subha Maity <smaity@gl-login1.arc-ts.umich.edu>
2020-08-11 22:38:43 -0400

    Let's see

--- compas_sensetive/create_fluctuations.py
+++ compas_sensetive/create_fluctuations.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     expts = ['sensr', 'reduction', 'baseline', 'project'] 
     data_index = range(1)#ends.shape[0])
     iteration = range(10)
-    lrs = [5e-2, 3e-2, 6e-2]
+    lrs = [2e-2, 3e-2, 1e-2, 5e-3, 2e-3]
 
     a = itertools.product(expts, data_index, iteration, lrs)
     b = [i for i in a]
